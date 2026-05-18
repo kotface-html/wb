7bdf82ac696259a3b2938afe1a533cb9ac6bd9c3
Taras Shemet <tarassemet1@gmail.com>
2026-05-18 19:37:38 +0200

    открывать и закрывать модальное окно , получать данные с строки для поиска (пока что толдько в консоль) :)

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+const cart = () => {
+
+    const cartBtn = document.querySelector('.button-cart')
+    const cart = document.getElementById('modal-cart')
+    const closeBtn = cart.querySelector('.modal-close')
+
+   
+    cartBtn.addEventListener('click', () => {
+        cart.style.display = 'flex'
+    })
+
+    closeBtn.addEventListener('click', () => {
+        cart.style.display = 'none'
+    })
+
+    cart.addEventListener('click', (event) => {
+        if (event.target === cart) {
+            cart.style.display = 'none'
+        }
+    })
+
+}
+
+cart()
+
+//// если чтото не понятно смотри в код элемента и посмотри какое айди и все что сзади 
